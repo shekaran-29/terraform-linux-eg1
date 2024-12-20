7687299958e37014fcb7df6f4392d5442fddb68d
HarshalST <162091332+HarshalST@users.noreply.github.com>
2024-12-20 11:11:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    linux = {
+      source = "TelkomIndonesia/linux"
+      version = "0.7.0"
+    }
+  }
+}
+
+provider "linux" {
+    host     = "192.168.235.131"
+    port     = 22
+    user     = "root"
+    password = "1"
+}
+
+resource "linux_file" "unnatifile" {
+    path = "/tmp/testdir"
+}
+
